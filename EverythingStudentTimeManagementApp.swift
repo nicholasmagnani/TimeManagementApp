a2b324ebcd442989a913ea168fda1958e00f8438
Nicholas Magnani <110574844+nicholasmagnani@users.noreply.github.com>
2022-08-06 14:03:14 -0400

    Add files via upload

---
+++ EverythingStudentTimeManagementApp.swift
@@ -0,0 +1,27 @@
+//
+//  EverythingStudentTimeManagementApp.swift
+//  EverythingStudentTimeManagement
+//
+//  Created by Nicholas Magnani on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct EverythingStudentTimeManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+                
+            ZStack(alignment: .center){
+                    //ContentView()
+                    Background()
+                    HomeScreen()
+                    DateChoice()
+                    
+
+
+                
+            }
+        }
+    }
+}
